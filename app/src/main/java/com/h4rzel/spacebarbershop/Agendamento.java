fb4882994e20cda8203a248558c639c53f2a7647
henrico <henricopp265@gmail.com>
2024-07-11 19:57:55 -0300

    feat: adicionar tela de agendamento

---
+++ app/src/main/java/com/h4rzel/spacebarbershop/Agendamento.java
@@ -0,0 +1,14 @@
+package com.h4rzel.spacebarbershop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Agendamento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agendamento);
+    }
+}
